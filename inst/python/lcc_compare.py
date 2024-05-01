fe9659d5a2a68a55125196b091f6c175699c479c
Patrick Jantz <pj276@nau.edu>
2024-05-01 06:56:54 -0700

    fix output name indexing

--- inst/python/lcc_compare.py
+++ inst/python/lcc_compare.py
@@ -135,5 +135,5 @@ for i, j in enumerate(nlist):
     comp = np.expand_dims(comp, axis=0)
     
     # Write to file
-    otiff = Path(odir) / ('s' + str(i) + '_lcc_comp.tif' )
+    otiff = Path(odir) / ('s' + str(i+1) + '_lcc_comp.tif' )
     cf.arrayToGeoTiff(comp, otiff, profile)
