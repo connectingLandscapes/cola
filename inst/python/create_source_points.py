b31e7320239adcb8f03ff8295ec5d3411931749c
Patrick Jantz <pj276@nau.edu>
2024-07-18 15:27:04 -0700

    fixed comment, last one shouldnt have been committed

--- inst/python/create_source_points.py
+++ inst/python/create_source_points.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jul 24 15:48:54 2023
-☺ We therefore mask out
+Script to create source points spatially 
+distributed according to habitat suitability.
+If the min or max values provided by the user
+are > or < the min and max values present in the
+raster, we assume the user wants points distributed
+within this restricted range. We therefore mask out
 values outside the provided range.
  
 @author: pj276
