c8351d4778d3631cd1a829e85f6219781f8b28a7
Patrick Jantz <pj276@nau.edu>
2025-11-04 19:21:54 -0700

    numerous significant changes

--- inst/python/lcc_hpc2_zarr.py
+++ inst/python/lcc_hpc2_zarr.py
@@ -1,24 +1,110 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 17 11:43:04 2025
+Created on Tue Oct 21 14:33:26 2025
 Read arrays pairwise from zarr file and calculate corridors
 @author: pj276
 """
 
 #%%
 # IMPORTS
-import sys
+import sys, os, shutil
+import zarr
 import cola_functions as cf
-import cola_zarr_functions as czf
+#import cola_zarr_functions as czf
 import numpy as np
 from scipy.ndimage import gaussian_filter
 import time
 from joblib import Parallel, delayed
-import shutil, os
+from collections import OrderedDict
 
+# Zarr file name
+dazarr = sys.argv[3]
+# Corridor tolerance
+corrTolerance = sys.argv[5]
+# Zarr file
+nodeidsFile = sys.argv[9]
+# Get nodeids
+nodeids = np.loadtxt(nodeidsFile, delimiter=',').astype(np.int32)
+
+# Get nodeids length
+nodeidsLen = len(nodeids)
+
+# --- LRU Array Cache (used per worker) ---
+class ArrayCache:
+    def __init__(self, max_size=4):
+        self.cache = OrderedDict()
+        self.max_size = max_size
+
+    def get(self, idx):
+        if idx in self.cache:
+            self.cache.move_to_end(idx)
+            return self.cache[idx]
+        else:
+            zf = zarr.open(dazarr, mode="r")
+            arr = zf[idx, :]
+            if len(self.cache) >= self.max_size:
+                self.cache.popitem(last=False)
+            self.cache[idx] = arr
+            return arr
+
+# --- Worker Function ---
+def process_batch(batch_pairs, nNodes, corrTolerance):
+    start_time = time.time()
+    cache = ArrayCache(max_size=4)
+    partial_sum = np.zeros((1,nodeidsLen), dtype=np.float64) #np.zeros((nNodes,), dtype=np.float64)
+    counter = 1
+    for i, j in batch_pairs:
+        lcc = cache.get(i) + cache.get(j)
+        lcc[np.isinf(lcc)] = np.nan
+        lcpVal = np.nanmin(lcc)
+        lcc = np.where(lcc <= lcpVal + corrTolerance + 0.001, 1, 0)
+        lcc[np.isnan(lcc)] = 0
+        partial_sum += lcc
+        if counter == 100:
+            et = time.time()
+            print(f"Time taken to process 100 corridors: {et - start_time:.2f} seconds", flush=True)            
+        counter += 1
+
+    end_time = time.time()
+    print(f"Time taken to process batch: {end_time - start_time:.2f} seconds", flush=True)
+    
+    return partial_sum
+
+# --- Worker Function ---
+# ptWeights is a dictionary where each point id has an associated weight.
+# Weights are the number of individuals in the population assocaited with that
+# point. Weights for connected points are multiplied. This is to ensure that
+# the corridor value corresponds to the number of trips taken by individuals
+# traveling between points.
+
+# For example, if point A has a weight of 3 and point B has a weight of 5,
+# the corridor between them will have a value of 15.
+
+def process_batch_weighted(batch_pairs, nNodes, corrTolerance, ptWeights):
+    start_time = time.time()
+    cache = ArrayCache(max_size=4)
+    partial_sum = np.zeros((1,nodeidsLen), dtype=np.float64) #np.zeros((nNodes,), dtype=np.float64)
+    counter = 1
+    for i, j in batch_pairs:
+        lcc = cache.get(i) + cache.get(j)
+        lcc[np.isinf(lcc)] = np.nan
+        lcpVal = np.nanmin(lcc)
+        cValue = ptWeights[i] * ptWeights[j]
+        lcc = np.where(lcc <= lcpVal + corrTolerance + 0.001, cValue, 0)
+        lcc[np.isnan(lcc)] = 0
+        partial_sum += lcc
+        if counter == 100:
+            et = time.time()
+            print(f"Time taken to process 100 corridors: {et - start_time:.2f} seconds", flush=True)            
+        counter += 1
+
+    end_time = time.time()
+    print(f"Time taken to process batch: {end_time - start_time:.2f} seconds", flush=True)
+    
+    return partial_sum
 def main() -> None:
     #%%
-   
+
     # Start timer
     tic = time.perf_counter()
     
@@ -30,7 +116,7 @@ def main() -> None:
     ofile = sys.argv[2]
 
     # Zarr file name
-    dazarr = sys.argv[3]
+    #dazarr = sys.argv[3]
     
     # Radius for gaussian smoother (in number of cells)
     # The size of the kernel on each side is 2*radius + 1
@@ -104,17 +190,21 @@ def main() -> None:
         nThreads = int(nThreads)
     except ValueError:
         print('Number of processors must be integer.')
-    
+   
     #%%
-    # Get reorder list
+    # Get reordered list
     reOrder = np.loadtxt(reOrderFile, delimiter=',').astype(np.int32)
-
-    # Get nodeids
-    nodeids = np.loadtxt(nodeidsFile, delimiter=',').astype(np.int32)
+    # Interleave pairs to reduce cache turnover and subsequent disk reads
+    tic1 = time.time()
+    reOrder = cf.interLeavePairs(reOrder)
+    toc1 = time.time()
+    print(f"Time taken to interleave pairs: {toc1 - tic1:.2f} seconds", flush=True)
     
-    # Get nodeids length
-    nodeidsLen = len(nodeids)
-        
+    # Subset pairs list if starting and ending indices are specified
+    # Use joblib to calculate corridors in parallel and sum on the fly
+    if (sci != 'None') and (eci != 'None') and (eci > sci):
+        reOrder = reOrder[sci:eci]
+
     #%%
     # Read grid to array
     # Assign crs if provided by user
@@ -134,31 +224,71 @@ def main() -> None:
     if profile['transform'][0] != np.abs(profile['transform'][4]):
         raise Exception('X and Y cell dimensions must be equal. Reformat resistance grid so that cell dimensions are the same in X and Y directions.')
 
-    #%%
-    # Use joblib to calculate corridors in parallel and sum on the fly
-    if (sci != 'None') and (eci != 'None') and (eci > sci):
-        startCorridor = sci
-        endCorridor = eci
-    else:
-        startCorridor = 0
-        endCorridor = len(reOrder)
-        
-    print("Summing corridors", flush=True)
-    res = Parallel(n_jobs=nThreads, return_as="generator_unordered", prefer="threads") (
-        delayed(czf.calcPathsMod)(reOrder[i], dazarr, corrTolerance) for i in range(startCorridor,endCorridor)
+    # If there are many corridors, do a test run to estimate total run time
+    # Run 100 corridors per thread
+    if len(reOrder) >= 5000:
+        print("You have a large number of corridors. Implementing a test run", flush=True)
+        print("to estimate total run time.", flush=True)
+        reOrder100 = reOrder[0:(100*nThreads)]
+        batch_size100 = 100
+        batches = [reOrder100[i:i + batch_size100] for i in range(0, len(reOrder100), batch_size100)]
+        tic1 = time.time()
+        results = Parallel(n_jobs=nThreads, return_as="generator_unordered", prefer="threads")(
+            delayed(process_batch)(batch, nodeidsLen, corrTolerance) for batch in batches
         )
+        testSum = np.zeros((1,nodeidsLen))
+        # Loop over generator and sum intermediates
+        for result in results:
+            testSum += result
+        toc1 = time.time()
+        tDiff = toc1-tic1
+        print(f'Processing speed is {tDiff/len(reOrder100)} seconds per corridor', flush=True)
+        print(f'Estimated time to run all corridors is {tDiff/len(reOrder100)*len(reOrder)} seconds', flush=True)
+        del testSum
+        del result
+        del reOrder100
+        del batch_size100
+        del batches
+        #testRun = np.zeros((1,nodeidsLen))
+        #tRun = []
+        #for i in [(0,100),(100,200),(200,300),(300,400)]:    
+        #    tic1 = time.time()
+        #    aa = process_batch(reOrder[i[0]:i[1]], nodeidsLen, corrTolerance)
+        #    testRun += aa
+        #    toc1 = time.time()
+        #    tDiff = toc1-tic1
+        #    tRun.append(tDiff)
+        #tRun = np.mean(tRun)
+        #print(f'Average time to run 100 corridors is {tRun} seconds')
+        #print(f'Estimated time to run all corridors is {tRun/100*len(reOrder)} seconds')
+        #del testRun
+        #del aa
+
+    # Equally distribute pairs among workers
+    batch_size = int(np.floor(len(reOrder)/nThreads))
+
+    # --- Divide Into Batches ---
+    batches = [reOrder[i:i + batch_size] for i in range(0, len(reOrder), batch_size)]
+    print("----", flush=True)
+    print(f"Processing {len(reOrder)} corridors in {len(batches)} batches.", flush=True)
+    
+    # --- Run In Parallel ---
+#    results = Parallel(n_jobs=num_workers, backend='multiprocessing', prefer="processes")(
+#        delayed(process_batch)(batch, nodeidsLen, corrTolerance) for batch in batches
+#    )
+    # Return as an unordered generator so that intermediate results
+    # can be summed as they become available
+    results = Parallel(n_jobs=nThreads, return_as="generator_unordered", prefer="threads")(
+        delayed(process_batch)(batch, nodeidsLen, corrTolerance) for batch in batches
+    )
+
+    # --- Final Sum ---
+#    lccSum = np.sum(results, axis=0)
+
     lccSum = np.zeros((1,nodeidsLen))
-    corridorCount = 1
-    tic1 = time.perf_counter()
-    for result in res:
+    # Loop over generator and sum intermediates
+    for result in results:
         lccSum += result
-        if corridorCount % 10000 == 0:
-            toc1 = time.perf_counter()
-            print(f'Finished corridor {corridorCount}', flush=True)
-            print(f"in {((toc1 - tic1)/60):0.4f} minutes", flush=True)
-            
-        corridorCount += 1
-        #print(f"Finished corridor {corridorCount}", flush=True)
     
     # Write to file
     print('Writing corridors to file', flush=True)
@@ -166,6 +296,10 @@ def main() -> None:
     # Create 1D zeros array to hold lcc values
     dArr = np.zeros([r.shape[0]*r.shape[1]], 'float32')
     # Update with lcc values
+    # Zarr file
+    nodeidsFile = sys.argv[9]
+    # Get nodeids
+    nodeids = np.loadtxt(nodeidsFile, delimiter=',').astype(np.int32)
     dArr[nodeids] = lccSum 
     # Reshape into rectangular array
     dArr = dArr.reshape(r.shape[0],r.shape[1])
@@ -193,20 +327,20 @@ def main() -> None:
     cf.arrayToGeoTiff(lccSmooth, ofile, profile)
     
     # If zarr files were created, delete them
-    if os.path.exists(dazarr):
-        try:
-            shutil.rmtree(dazarr)
-            print(f"Successfully deleted Zarr store at: {dazarr}")
-        except OSError as e:
-            print(f"Error deleting Zarr store at {dazarr}: {e}")
-    else:
-        print(f"Zarr store not found at: {dazarr}")
+#    if os.path.exists(dazarr):
+#        try:
+#            shutil.rmtree(dazarr)
+#            print(f"Successfully deleted Zarr store at: {dazarr}")
+#        except OSError as e:
+#            print(f"Error deleting Zarr store at {dazarr}: {e}")
+#    else:
+#        print(f"Zarr store not found at: {dazarr}")
 
-    # Delete csv files
-    if os.path.exists(reOrderFile):
-        os.remove(reOrderFile)
-    if os.path.exists(nodeidsFile):
-        os.remove(nodeidsFile)
+#    # Delete csv files
+#    if os.path.exists(reOrderFile):
+#        os.remove(reOrderFile)
+#    if os.path.exists(nodeidsFile):
+#        os.remove(nodeidsFile)
 
     toc = time.perf_counter()
     print(f"Calculating least cost corridors took {toc - tic:0.4f} seconds", flush=True)
